e04437618d9bd60232a78b03a65111472d7d51bd
Kevin Fu <kevin.fu808@gmail.com>
2022-07-05 15:25:36 -0500

    Applying formatter changes

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use serde::{Deserialize};
+use serde::Deserialize;
 
 #[derive(Deserialize, Debug)]
 struct VenmoRequest {
@@ -12,7 +12,7 @@ fn main() {
     let example_venmo_request = VenmoRequest {
         _amount_in_cents: 1000,
         _friend_id: String::from("example_friend_id"),
-        _note_text: String::from("test text")
+        _note_text: String::from("test text"),
     };
     println!("{:#?}", example_venmo_request);
 }
