657168c3ff25331442283346dd56f80e0114ab30
Kevin Fu <kevin.fu808@gmail.com>
2022-07-05 15:13:47 -0500

    Starting commit with VenmoRequest object logged

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use serde::{Deserialize};
+
+#[derive(Deserialize, Debug)]
+struct VenmoRequest {
+    _amount_in_cents: u32,
+    _friend_id: String,
+    _note_text: String,
+}
+
+fn main() {
+    println!("Hello, world!");
+    let example_venmo_request = VenmoRequest {
+        _amount_in_cents: 1000,
+        _friend_id: String::from("example_friend_id"),
+        _note_text: String::from("test text")
+    };
+    println!("{:#?}", example_venmo_request);
+}
