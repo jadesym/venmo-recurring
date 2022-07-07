d91ca3262e11bb33d5d7e41263e49ba191cde785
Kevin Fu <kevin.fu808@gmail.com>
2022-07-07 13:17:27 -0500

    Completed implementation (untested) with dummy variable values for venmo payment creation; pending lint errors

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 mod venmo;
 
 fn main() {
-    println!("Hello, world!");
-    let example_venmo_request = venmo::create_venmo_request();
-    println!("{:#?}", example_venmo_request);
+    let created_venmo_payment_charge_result =
+        venmo::create_venmo_payment_charge(venmo::VenmoCharge {
+            charge_amount_in_cents: 1000,
+            target_unique_id: venmo::VenmoPaymentTargetUniqueId::UserId(String::from(
+                "test_user_id",
+            )),
+            note_text: String::from("example note text"),
+        });
+    println!("{:#?}", created_venmo_payment_charge_result);
 }
--- src/venmo/mod.rs
+++ src/venmo/mod.rs
@@ -1,16 +1,201 @@
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
+use std::fmt;
+use std::io::Error;
+use ureq::Error as UreqError;
 
-#[derive(Deserialize, Debug)]
-pub struct VenmoRequest {
-    _amount_in_cents: u32,
-    _friend_id: String,
-    _note_text: String,
+const VENMO_BASE_URL: &str = "https://api.venmo.com/v1";
+const VENMO_PAYMENTS_PATH: &str = "/payments";
+const PRIVATE_AUDIENCE_PARAM_VALUE: &str = "private";
+
+#[derive(PartialEq, Deserialize)]
+struct VenmoPaymentTarget {
+    // TODO | figure out how to deserialize into some form of enum string type
+    #[serde(rename = "type")]
+    kind: String,
+    phone: Option<String>,
+    email: Option<String>,
+    user: Option<String>,
 }
 
-pub fn create_venmo_request() -> VenmoRequest {
-    VenmoRequest {
-        _amount_in_cents: 1000,
-        _friend_id: String::from("example_friend_id"),
-        _note_text: String::from("test text"),
+#[derive(PartialEq, Deserialize)]
+struct VenmoPaymentActor {
+    id: String,
+}
+
+#[derive(PartialEq, Deserialize)]
+struct VenmoPayment {
+    id: String,
+    status: String,
+    action: String,
+}
+
+#[derive(PartialEq, Deserialize)]
+struct VenmoChargeCreateResponseData {
+    balance: f32,
+    payment: VenmoPayment,
+}
+
+#[derive(PartialEq, Deserialize)]
+#[serde(untagged)]
+enum VenmoChargeCreateResponse {
+    DataResponse {
+        data: VenmoChargeCreateResponseData,
+    },
+    ErrorResponse {
+        error: VenmoChargeCreateResponseError,
+    },
+}
+
+impl fmt::Display for VenmoChargeCreateError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            VenmoChargeCreateError::JsonParseError(ref e) => {
+                write!(f, "Error parsing JSON: {:?}", e)
+            }
+            VenmoChargeCreateError::CallResponseError(ref e) => {
+                write!(f, "Error from response: {:?}", e)
+            }
+            VenmoChargeCreateError::WithinResponseError(ref e) => write!(
+                f,
+                "Error within response with code [{:?}] and message: {:?}",
+                e.code, e.message
+            ),
+        }
     }
+}
+
+impl From<Error> for VenmoChargeCreateError {
+    fn from(e: Error) -> Self {
+        VenmoChargeCreateError::JsonParseError(e)
+    }
+}
+
+impl From<UreqError> for VenmoChargeCreateError {
+    fn from(e: UreqError) -> Self {
+        VenmoChargeCreateError::CallResponseError(e)
+    }
+}
+
+#[derive(Serialize)]
+struct VenmoChargeCreateParameters {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    phone: Option<String>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    email: Option<String>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    user_id: Option<String>,
+
+    amount: f64,
+
+    access_token: String,
+
+    note: String,
+
+    audience: String,
+}
+
+fn create_venmo_charge_create_params(
+    access_token: String,
+    charge_amount_in_cents: u32,
+    venmo_unique_id: VenmoPaymentTargetUniqueId,
+    note_text: String,
+) -> VenmoChargeCreateParameters {
+    let amount: f64 = f64::from(charge_amount_in_cents) / 100f64;
+    let audience: String = String::from(PRIVATE_AUDIENCE_PARAM_VALUE);
+
+    match venmo_unique_id {
+        VenmoPaymentTargetUniqueId::PhoneNumber(phone_number) => VenmoChargeCreateParameters {
+            phone: Some(phone_number),
+            email: None,
+            user_id: None,
+            amount: amount,
+            access_token: access_token,
+            note: note_text,
+            audience: audience,
+        },
+        VenmoPaymentTargetUniqueId::Email(email) => VenmoChargeCreateParameters {
+            phone: None,
+            email: Some(email),
+            user_id: None,
+            amount: amount,
+            access_token: access_token,
+            note: note_text,
+            audience: audience,
+        },
+        VenmoPaymentTargetUniqueId::UserId(user_id) => VenmoChargeCreateParameters {
+            phone: None,
+            email: None,
+            user_id: Some(user_id),
+            amount: amount,
+            access_token: access_token,
+            note: note_text,
+            audience: audience,
+        },
+    }
+}
+
+fn execute_payment_charge_request(
+    charge_amount_in_cents: u32,
+    venmo_unique_id: VenmoPaymentTargetUniqueId,
+    note_text: String,
+) -> Result<VenmoChargeCreateResponseData, VenmoChargeCreateError> {
+    let params: VenmoChargeCreateParameters = create_venmo_charge_create_params(
+        String::from("<TO_BE_REPLACED_ACCESS_TOKEN>"),
+        charge_amount_in_cents,
+        venmo_unique_id,
+        note_text,
+    );
+
+    let mut url: String = VENMO_BASE_URL.to_owned();
+    url.push_str(VENMO_PAYMENTS_PATH);
+    let url: &str = url.as_str();
+
+    let response = ureq::post(url).send_json(serde_json::json!(params))?;
+
+    let parsed_json_response: VenmoChargeCreateResponse = response.into_json()?;
+
+    match parsed_json_response {
+        VenmoChargeCreateResponse::DataResponse { data } => Ok(data),
+        VenmoChargeCreateResponse::ErrorResponse { error } => {
+            Err(VenmoChargeCreateError::WithinResponseError(error))
+        }
+    }
+}
+
+#[derive(Debug, Deserialize, PartialEq)]
+pub struct VenmoChargeCreateResponseError {
+    message: String,
+    code: String,
+}
+
+#[derive(Debug)]
+pub enum VenmoChargeCreateError {
+    CallResponseError(ureq::Error),
+    JsonParseError(std::io::Error),
+    WithinResponseError(VenmoChargeCreateResponseError),
+}
+
+#[derive(Debug)]
+pub struct VenmoCharge {
+    pub charge_amount_in_cents: u32,
+    pub target_unique_id: VenmoPaymentTargetUniqueId,
+    pub note_text: String,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum VenmoPaymentTargetUniqueId {
+    PhoneNumber(String),
+    Email(String),
+    UserId(String),
+}
+
+pub fn create_venmo_payment_charge(venmo_charge: VenmoCharge) -> Option<VenmoChargeCreateError> {
+    let VenmoCharge {
+        charge_amount_in_cents,
+        target_unique_id,
+        note_text,
+    } = venmo_charge;
+    execute_payment_charge_request(charge_amount_in_cents, target_unique_id, note_text).err()
 }
